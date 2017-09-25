a8654213816e487c73164dc0eef5437f70b6d392
Bove <ribohe94@gmail.com>
2017-09-25 01:29:09 -0600

    add hangman ascii stages and paint function

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,11 +1,30 @@
 import sys
+import os
 
-def read(filename):
-    words = open(filename, mode='rt', encoding='utf-8')
-    print(words.read())
+def read(filename, lineNumber):
+    words = open(filename, mode='rt', encoding='utf-8').readlines()
+    if(lineNumber <= len(words)-1 & lineNumber >= 0):
+        print(words[lineNumber].rstrip())
+        return True
+    else:
+        print("Index out of bounds")
+        return False
+
+def paint(hangManLevel):
+    d = {
+    0:"hangman_base.txt",
+    1:"hangman_pole.txt",
+    2:"hangman_head.txt",
+    3:"hangman_body.txt",
+    4:"hangman_legs.txt",
+    5:"hangman_complete.txt",
+    }
+    hangman = open("hangman_ascii/" + d[hangManLevel], mode='rt', encoding='utf-8')
+    print(hangman.read())
 
 if (__name__ == '__main__'):
     try:
-        read(sys.argv[1])
+        read(sys.argv[1], int(sys.argv[2]))
     except FileNotFoundError:
         print("File reading failed!")
+    print(paint(0))
