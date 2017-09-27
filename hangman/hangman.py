ad975d217700aa0fbcaa83106546eafe10e2c6a0
Bove <ribohe94@gmail.com>
2017-09-26 19:59:53 -0600

    Create game loop

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,16 +1,19 @@
 import sys
 import os
+import random
+import pdb
 
-def read(filename, lineNumber):
+gameWord = None;
+outputVal = None;
+def read(filename):
     words = open(filename, mode='rt', encoding='utf-8').readlines()
-    if(lineNumber <= len(words)-1 & lineNumber >= 0):
-        print(words[lineNumber].rstrip())
-        return True
-    else:
-        print("Index out of bounds")
-        return False
+    lineNumber = int(random.uniform(0, len(words)));
+    global gameWord
+    gameWord = words[lineNumber].rstrip();
+    print(gameWord)
+    return True
 
-def paint(hangManLevel):
+def paint(hangManStatus):
     d = {
     0:"hangman_base.txt",
     1:"hangman_pole.txt",
@@ -19,12 +22,48 @@ def paint(hangManLevel):
     4:"hangman_legs.txt",
     5:"hangman_complete.txt",
     }
-    hangman = open("hangman_ascii/" + d[hangManLevel], mode='rt', encoding='utf-8')
+    try:
+        hangman = open("hangman_ascii/" + d[hangManStatus], mode='rt', encoding='utf-8')
+    except KeyError:
+        print("Key doesn't exist")
     print(hangman.read())
+
+def gameLoop():
+    round = 0;
+    gameStatus = True;
+    hangmanStatus = 0;
+    guess = '_' * len(gameWord);
+    guessAttempts = [];
+    global outputVal
+    outputVal = '_' * len(gameWord);
+    while(gameStatus):
+        os.system('clear')
+        if((validateWord(guess)==False) & (round != 0)):
+            hangmanStatus = hangmanStatus + 1;
+        paint(hangmanStatus)
+        guess = input("Guess letter or string")
+        guessAttempts = guessAttempts + list(set(guess));
+        round = round + 1;
+
+
+def validateWord(guess):
+    global outputVal
+    wordMatch = False;
+    for w in list(guess):
+        for i, j in enumerate(list(gameWord)):
+            if j == w:
+                outputVal = list(outputVal);
+                outputVal[i] = w;
+                wordMatch = True;
+    print(''.join(outputVal));
+    return wordMatch;
+
+
 
 if (__name__ == '__main__'):
     try:
-        read(sys.argv[1], int(sys.argv[2]))
+        read(sys.argv[1]);
+        gameLoop();
     except FileNotFoundError:
         print("File reading failed!")
     print(paint(0))
