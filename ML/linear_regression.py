1957bc38c5cdd983b6f8e23f31c6250bdf96b131
Bove <ribohe94@gmail.com>
2017-10-19 00:19:11 -0600

    Create cost Function hx is being treated as an ndimensional array, have to find a cleaner way to substract y

--- ML/linear_regression.py
+++ ML/linear_regression.py
@@ -1,7 +1,10 @@
 import numpy as np
 
 def getCost(X, y, weights):
-    pass
+    m = np.size(X,0)
+    hx = X.dot(weights)
+    cost = (1/(2*m)) * np.sum((hx[:,0] - y)**2)
+    return cost
 
 def main():
    #Load data x=population, y=profit
@@ -14,8 +17,11 @@ def main():
     #Hyperparameters!
     m = np.size(data,0)
     data=np.insert(data,0,1,axis=1) #Adding the bias unit!
+    X = data[:,0:2]
+    y = data[:,2]
     iterations = 1500
     alpha = 0.01
+    print(getCost(X, y, weights))
 
 
 
