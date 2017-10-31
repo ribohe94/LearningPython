2355387268c5cc8e3febfc9ae8f6028a86d29e42
Bove <ribohe94@gmail.com>
2017-10-31 01:22:29 -0600

    Remove redundant lines

--- ML/logistic_regression.py
+++ ML/logistic_regression.py
@@ -8,26 +8,46 @@ def sigmoid(z):
 
 def costFunction(X, y, weights):
     m = np.size(X,0)
-    hx = sigmoid(X.dot(weights))
-    hx = hx.astype(np.float64)
-    return (1 / m) * sum(-y * np.log10(hx) - (1 - y) * np.log10(1 - hx))
+    activation = sigmoid(X.dot(weights))
+    activation = activation.astype(np.float64)
+    return (1 / m) * sum(-y * np.log10(activation) - (1 - y) * np.log10(1 - activation))
 
-def costFunctionGradient(X, y, weights):
+def costFunctionactivation(X, y, weights):
     m = np.size(X,0)
-    gradient = sigmoid(X.dot(weights)) - y
-    gradient = gradient.astype(np.float64)
-    temp_weights[0] = weights[0] - (1/m) * np.sum(gradient)
+    activation = sigmoid(X.dot(weights)) - y
+    activation = activation.astype(np.float64)
+    temp_weights = weights
+    temp_weights[0] = (1/m) * np.sum(activation)
+    y = np.reshape(y, [np.size(y),1])
     
-    for j in range(1,n):
-        temp_weights[j] = weights[j] - alpha * (1/m) * np.sum(gradient * X[:, j:j+1])
+    for j in range(1, np.size(weights)):
+        temp_weights[j] = (1/m) * np.sum(activation * X[:, j:j+1])
+    
+    print(costFunction(X,y,temp_weights))
+
+    return temp_weights
+
+def plotData(data, xAxis, yAxis):
+    survived = data[data['Survived'].isin([1])] #Passengers that survived
+    ded = data[data['Survived'].isin([0])] #Slept with the fishes
+    
+    fig, ax = plt.subplots(figsize=(16,9))  
+    ax.scatter(survived[xAxis], survived[yAxis], s=10, c='b', marker='o', label='Survived')
+    ax.scatter(ded[xAxis],ded[yAxis], s=10, c='r', marker='x', label='Ded')
+    ax.legend()
+    ax.set_xlabel(xAxis)  
+    ax.set_ylabel(yAxis)
+    plt.show()
 
 
 def main():
     #Load data
-    data = pd.read_csv('titanic.csv', header=0, names=['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked'], skipinitialspace=True, quotechar='"', skiprows=1)
+    headers=['PassengerId','Survived','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked']
+    data = pd.read_csv('titanic.csv', header=0, names=headers, skipinitialspace=True, quotechar='"', skiprows=1)
     
     #x = passenger class
-    X = pd.DataFrame(data['Pclass'])
+    X = pd.DataFrame(data[['Pclass', 'Fare']])
+    X = (X - np.mean(X)) / np.std(X) #Normalizing data
     X.insert(loc=0, column='Bias', value=1) #Adding column of 1's for the bias units!
     #y = slept with the fishes
     y = data['Survived']
@@ -35,19 +55,14 @@ def main():
     #Initializing weights at 0!
     weights = np.zeros((np.size(X,1),1))
     
-    survived = data[data['Survived'].isin([1])]
-    ded = data[data['Survived'].isin([0])]
-    
-    fig, ax = plt.subplots(figsize=(16,9))  
-    ax.scatter(survived['PassengerId'], survived['Pclass'], s=10, c='b', marker='o', label='Survived')  
-    ax.scatter(ded['PassengerId'],ded['Pclass'], s=10, c='r', marker='x', label='Ded')
-    ax.legend()
-    ax.set_xlabel('Passenger sequence number')  
-    ax.set_ylabel('Age')
-    plt.show()
-    
+    #plotData(data, 'PassengerId', 'Pclass')
+        
     #Calculate cost function
+    X = np.array(X.values) #Turn X into an ndarray for ease of usage
+    y = np.array(y.values) #Turn y into an ndarray for ease of usage
+    y = np.reshape(y, [np.size(y),1]) #Reshape it into (size,1) nd array instead of (size,)
     print(costFunction(X, y, weights))
+    costFunctionGradient(X, y, weights)
 
 if __name__ == '__main__':
     main()
